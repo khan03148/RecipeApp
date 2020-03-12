739278837d4dd8d4ef583e996251759e31310041
Jahangir khan <khan03148@gmail.com>
2020-03-12 12:14:31 +0100

    Updates

---
+++ precache-manifest.9d6b0c74bd98b10edfcd73dacd91a36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d90c963e8454da30408bc2f63c120f",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "aed5cde16a37659c725c",
+    "url": "/RecipeApp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "70de61943338f59bbb61",
+    "url": "/RecipeApp/static/css/main.9092f6f4.chunk.css"
+  },
+  {
+    "revision": "aed5cde16a37659c725c",
+    "url": "/RecipeApp/static/js/2.e987ddfd.chunk.js"
+  },
+  {
+    "revision": "70de61943338f59bbb61",
+    "url": "/RecipeApp/static/js/main.25936962.chunk.js"
+  },
+  {
+    "revision": "f06fa8d91e22f2e7c843",
+    "url": "/RecipeApp/static/js/runtime-main.2f40ce4d.js"
+  }
+]);
